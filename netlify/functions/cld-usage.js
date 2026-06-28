80c28b641652b16c643d5c8bc2a1899bebc330fd
PKN7KR <112880738+PKN7KR@users.noreply.github.com>
2026-06-29 00:00:13 +0900

    Update cld-usage.js

--- netlify/functions/cld-usage.js
+++ netlify/functions/cld-usage.js
@@ -1,1 +1,17 @@
 
+const https = require('https');
+
+exports.handler = async (event, context) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Methods': 'GET, OPTIONS',
+    'Content-Type': 'application/json',
+  };
+
+  if (event.httpMethod === 'OPTIONS') return { statusCode: 204, headers, body: '' };
+  if (event.httpMethod !== 'GET') return { statusCode: 405, headers, body: JSON.stringify({ error: 'Method not allowed' }) };
+
+  con
